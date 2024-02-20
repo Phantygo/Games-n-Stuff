d0ea41ed3d8a11a1e834fc1987fe7e2758023e32
Phantygo <82779463+Phantygo@users.noreply.github.com>
2024-02-20 15:04:59 -0500

    Update BlackJack2.0.py

--- BlackJack2.0.py
+++ BlackJack2.0.py
@@ -2,7 +2,7 @@ import random
 import sys
 
 playing = True
-# Suit, Rank, and Values
+# Suit, Rank, and Values.
 
 values = {"Two": 2, "Three": 3, "Four": 4, "Five": 5, "Six": 6, "Seven": 7, "Eight": 8, "Nine": 9, "Ten": 10, "Jack": 10, "Queen": 10, "King": 10, "Ace": 1}
 ranks = ("Two", "Three", "Four", "Five", "Six","Seven","Eight","Nine", "Ten", "Jack", "Queen", "King", "Ace")
